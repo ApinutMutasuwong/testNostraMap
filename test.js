87b8ec080d117bb0c9265aeddd87e35e9eee52f8
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-10 16:27:38 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors'); // Import the cors middleware
+const app = express();
+const port = 80;
+
+// Use the cors middleware
+app.use(cors());
+
+// Define a sample API endpoint
+app.get('/api/resource', (req, res) => {
+  res.json({ message: 'Hello from your API!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
