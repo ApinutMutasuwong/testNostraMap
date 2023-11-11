19b28a5baa564793065db019ada8ab1e41e6a775
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-11 20:18:17 +0700

    Delete src directory

--- src/components/Main/Section/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="main-footer">
-      <p className="copyright">
-        Copyright © 2023 by Me, Inc. All rights reserved.
-      </p>
-    </footer>
-  );
-}
