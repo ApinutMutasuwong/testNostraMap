9cd2bcb3dca3158bfffb53dfad6b4d1fff616f37
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-11 21:24:12 +0700

    Update Test.js

--- src/Test.js
+++ src/Test.js
@@ -2,31 +2,27 @@ import React from 'react';
 
 function Test() {
   const runScript = () => {
-    const WebSocket = require('ws');
-    console.log("Marai")
-    // Replace 'your-ec2-public-ip' with the actual public IP or DNS of your EC2 instance
     const serverAddress = 'ws://3.94.102.73:4000';
-
     const socket = new WebSocket(serverAddress);
 
-    socket.on('open', () => {
-    console.log('WebSocket connection opened');
-
-    // Send a message to the server after the connection is opened
-    socket.send('Hello, server!');
+    socket.addEventListener('open', () => {
+      console.log('WebSocket connection opened');
+      // Send a message to the server after the connection is opened
+      socket.send('Hello, server!');
     });
 
-    socket.on('message', (data) => {
-    console.log('Received message from server:', data);
+    socket.addEventListener('message', (event) => {
+      console.log('Received message from server:', event.data);
     });
 
-    socket.on('close', (code, reason) => {
-    console.log('WebSocket connection closed:', code, reason);
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:', event.code, event.reason);
     });
 
-    socket.on('error', (error) => {
-    console.error('WebSocket error:', error);
+    socket.addEventListener('error', (error) => {
+      console.error('WebSocket error:', error);
     });
+
 
     alert('Script executed!');
   };
@@ -40,3 +36,4 @@ function Test() {
 }
 
 export default Test;
+
