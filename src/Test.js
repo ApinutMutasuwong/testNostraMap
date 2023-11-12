4a2e59dc6d77d2083abcd7da032926520c202cf7
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-12 17:31:36 +0700

    Update Test.js

--- src/Test.js
+++ src/Test.js
@@ -3,43 +3,29 @@ function Test() {
   const runScript = () => {
     console.log("start")
     // Create a new WebSocket connection
-    const socket = new WebSocket('wss://54.172.56.141:4000');
+    const nativeWebSocket = new WebSocket("wss://127.0.0.1:4000");
+    setWs(nativeWebSocket);
 
-    // Connection opened
-    socket.addEventListener('open', (event) => {
-      console.log('Connected to server');
-      socket.send('Hello Server!');
-    });
+    nativeWebSocket.onopen = () => {
+      console.log("WebSocket connection established on search file");
+    };
 
-    // Listen for messages
-    socket.addEventListener('message', (event) => {
-      console.log('Message from server:', event.data);
-    });
+    nativeWebSocket.onmessage = (event) => {
+      const data = event.data;
+      console.log(`Received WebSocket data: ${data}`);
+      // Handle data received from the server (e.g., map updates)
+    };
 
-    // Connection closed
-    socket.addEventListener('close', (event) => {
-      console.log('Server connection closed', event);
-    });
+    nativeWebSocket.onclose = () => {
+      console.log("WebSocket connection closed on search file");
+    };
 
+    console.log("end");
 
-   // Handle errors
-    socket.addEventListener('error', (event) => {
-      console.error('WebSocket Error:', event);
-      console.log('Ready state:', socket.readyState);
-      console.log('Buffered amount:', socket.bufferedAmount);
-      // Add additional information if available
-      if (event.message) {
-        console.error('Error message:', event.message);
-      }
-    });
-
-
-
-    // Send a message to the server
-    function sendMessage(message) {
-      socket.send(message);
-    }
-
+    // Clean up the WebSocket connection when the component unmounts
+    return () => {
+      nativeWebSocket.close();
+    };
 
 
     console.log("end")
