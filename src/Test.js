ae576a4a25503fb7541ef83ff4dc2ae114dcb7b3
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-12 18:54:50 +0700

    Update Test.js

--- src/Test.js
+++ src/Test.js
@@ -8,7 +8,7 @@ function Test() {
     console.log("start");
 
     // Create a new WebSocket connection
-    const nativeWebSocket = new WebSocket("ws://35.153.142.248:4000");
+    const nativeWebSocket = new WebSocket("wss://35.153.142.248:4000");
     setWs(nativeWebSocket);
 
     nativeWebSocket.onopen = () => {
